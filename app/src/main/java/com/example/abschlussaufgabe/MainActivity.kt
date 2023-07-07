4bc1835f50aafd2daa32563ec592234ce38a58b3
Nouiaskarnian <nouiaskarnian@gmail.com>
2023-07-07 10:11:08 +0200

    AbschlussAufgaben

--- app/src/main/java/com/example/abschlussaufgabe/MainActivity.kt
+++ app/src/main/java/com/example/abschlussaufgabe/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.abschlussaufgabe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.lifecycle.ReportFragment.Companion.reportFragment
+
 import androidx.navigation.findNavController
 import com.google.android.material.bottomnavigation.BottomNavigationItemView
 
@@ -11,12 +11,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         val navController = findNavController(R.id.navHost)
         findViewById<BottomNavigationItemView>(R.id.bottomNav)
-
+            //.setupWithNavController(navController)
 
     }
 
+
 }
+
+
 
 
